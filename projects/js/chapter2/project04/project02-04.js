9eeb1b4bde7a5fe32c2ab080f89fed87cf3e87b6
TetelestaiDerp <davyd.yendrys@stu.cvccworks.edu>
2025-10-22 13:35:50 -0400

    Add files via upload

---
+++ projects/js/chapter2/project04/project02-04.js
@@ -0,0 +1,60 @@
+// Function to display a numeric value as a text string in the format $##.## 
+/*
+   JavaScript 7th Edition
+   Chapter 2
+   Project 02-04
+
+   Application to calculate the cost of a restaurant order plus tax
+   Author: davyd
+   Date:   yendrys
+
+   Filename: project02-04.js
+*/
+
+// Format number as currency
+function formatCurrency(value) 
+{
+   return "$" + value.toFixed(2);
+}
+
+// Constants for item prices
+const CHICKEN_PRICE = 10.95;
+const HALIBUT_PRICE = 13.95;
+const BURGER_PRICE = 9.95;
+const SALMON_PRICE = 18.95;
+const SALAD_PRICE = 7.95;
+const SALES_TAX = 0.07;
+
+// Event handlers for all checkboxes
+document.getElementById("chicken").onclick = calcTotal;
+document.getElementById("halibut").onclick = calcTotal;
+document.getElementById("burger").onclick = calcTotal;
+document.getElementById("salmon").onclick = calcTotal;
+document.getElementById("salad").onclick = calcTotal;
+
+// Function to calculate totals
+function calcTotal() 
+{
+   let cost = 0;
+
+   let buyChicken = document.getElementById("chicken").checked;
+   let buyHalibut = document.getElementById("halibut").checked;
+   let buyBurger = document.getElementById("burger").checked;
+   let buySalmon = document.getElementById("salmon").checked;
+   let buySalad = document.getElementById("salad").checked;
+
+   cost += buyChicken ? CHICKEN_PRICE : 0;
+   cost += buyHalibut ? HALIBUT_PRICE : 0;
+   cost += buyBurger ? BURGER_PRICE : 0;
+   cost += buySalmon ? SALMON_PRICE : 0;
+   cost += buySalad ? SALAD_PRICE : 0;
+
+   document.getElementById("foodTotal").innerHTML = formatCurrency(cost);
+
+   let tax = cost * SALES_TAX;
+   document.getElementById("foodTax").innerHTML = formatCurrency(tax);
+
+   let totalCost = cost + tax;
+   document.getElementById("totalBill").innerHTML = formatCurrency(totalCost);
+}
+
