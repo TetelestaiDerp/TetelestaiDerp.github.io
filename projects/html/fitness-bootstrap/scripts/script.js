8db7d84962cbaa41a22e37260cc16b1e6a41f711
TetelestaiDerp <davyd.yendrys@stu.cvccworks.edu>
2025-12-15 13:06:12 -0500

    Add files via upload

---
+++ projects/html/fitness-bootstrap/scripts/script.js
@@ -0,0 +1,8 @@
+/*
+    Student Name: Davyd Yendrys
+    File Name: script.js
+    Date: Decemeber 12, 2025
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {$('.hero').height($(window).height());});
