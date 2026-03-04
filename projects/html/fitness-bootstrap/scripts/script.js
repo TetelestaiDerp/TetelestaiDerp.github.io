0e1e586f4b695f4c822e792561188385cb0db770
TetelestaiDerp <davyd.yendrys@stu.cvccworks.edu>
2026-03-04 14:13:42 -0500

    Delete projects/html directory

--- projects/html/fitness-bootstrap/scripts/script.js
+++
@@ -1,8 +0,0 @@
-/*
-    Student Name: Davyd Yendrys
-    File Name: script.js
-    Date: Decemeber 12, 2025
-*/
-
-//jQuery for hero image to consume the header window space
-$(document).ready(function() {$('.hero').height($(window).height());});
