3f2c37fb2bc141f5235f8caa53bec7aeb11e8a67
TetelestaiDerp <davyd.yendrys@stu.cvccworks.edu>
2026-03-13 14:22:29 -0400

    Add files via upload

--- projects/js/chapter5/project02/project05-02.js
+++ projects/js/chapter5/project02/project05-02.js
@@ -4,32 +4,34 @@
       Project 05-02
 
       Project to move images between a photo bucket and photo list.
-      Author: 
+      Author: davyd
       Date: 3/11/2026  
 
       Filename: project05-02.js
 */
 
 //delare variables used
+"use strict";
+
 let images = document.getElementsByTagName("img");
 let photoBucket = document.getElementById("photo_bucket");
 let photoList = document.getElementById("photo_list");
 
 for (let i = 0; i < images.length; i++) 
-      {
-      images[i].onclick = function() 
-      {
-            if(this.parentElement.id === "photo_bucket") 
-                  {
-                  let newItem = document.createElement("li");
-                  photoList.appendChild(newItem);
-                  newItem.appendChild(this);
-            } 
-            else 
-                  {
-                  let oldItem = this.parentElement;
-                  photoBucket.appendChild(this);
-                  oldItem.parentElement.removeChild(oldItem);
-            }
-      }
+{
+    images[i].onclick = function () 
+    {
+        if (this.parentElement.id === "photo_bucket") 
+            {
+            let newItem = document.createElement("li");
+            photoList.appendChild(newItem);
+            newItem.appendChild(this);
+        } 
+        else 
+            {
+            let oldItem = this.parentElement;
+            photoBucket.appendChild(this);
+            oldItem.remove();
+        }
+    };
 }
